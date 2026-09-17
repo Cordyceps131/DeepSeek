3e7e1f68b406a125100c2286ec3319f812f78bb6
Cordyceps <andreproenca13@gmail.com>
2026-09-17 17:07:37 +0100

    fix: http to https

--- app-filmes/app.js
+++ app-filmes/app.js
@@ -2,7 +2,7 @@ import { apiKey } from './api.js';
 
 const buscarPesquisa = async (userInput) => {
     try {
-        const resposta = await fetch(`http://www.omdbapi.com/?s=${userInput}&apikey=${apiKey}`);
+        const resposta = await fetch(`https://www.omdbapi.com/?s=${userInput}&apikey=${apiKey}`);
         const pesquisa = await resposta.json();
 
         return pesquisa;
@@ -73,7 +73,7 @@ btn.addEventListener('click', async (e) => {
 
 const buscarFiltro = async (nome, ano) => {
     try {
-        const resposta = await fetch(`http://www.omdbapi.com/?s=${nome}&y=${ano}&apikey=${apiKey}`);
+        const resposta = await fetch(`https://www.omdbapi.com/?s=${nome}&y=${ano}&apikey=${apiKey}`);
         const pesquisa = await resposta.json();
         console.log(pesquisa.totalResults);
 
