fcbccf9b91d2463ba6e16985321fbb1a8348860a
Cordyceps <andreproenca13@gmail.com>
2026-09-22 11:38:19 +0100

    feat: addTask event

--- to-do_list/app.js
+++ to-do_list/app.js
@@ -22,5 +22,33 @@ const render = (tarefas) => {
     ).join('');
 }
 
+const atualizarContador = () => {
+    const total = tarefas.length;
+    const concluidas = tarefas.filter(t => t.concluida).length;
+    const txt = `${total} de ${concluidas} concluídas`;
+    contador.textContent = total > 0 ? txt : '';
+}
+
+
+inputBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+    let input = tarefa.value;
+    if (!input) {
+        msg.textContent = 'Introduz a tarefa que queres adicionar';
+        tarefa.focus();
+        return;
+    }
+
+    const novaTarefa = { id: proximoID++, texto: input, concluida: false };
+    tarefas.push(novaTarefa);
+    tarefa.value = '';
+    tarefa.focus();
+    render(tarefas);
+    atualizarContador();
+    
+});
+
+
+
 
 
